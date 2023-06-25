f7a35cb7bd6c6ff31e1429c394c0c03c6cc67cd1
artem-ltv <60981162+artem-ltv@users.noreply.github.com>
2023-06-25 11:41:13 +0300

    add sum function

--- funcs.py
+++ funcs.py
@@ -1,6 +1,3 @@
-num1 = 15
-num2 = 12
-
-sum = num1 + num2
-
-print("Сумма", num1, "и", num2, "равна", sum)
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from funcs import sum
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,3 +10,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print(sum(3, 5))
